d841a8c94975bad41f2267dd3fac35ad99496a53
erankatz <eranki.katz@gmail.com>
2023-01-14 19:11:41 +0200

    deleted log

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const firebase = require('firebase');
 const config = require('./config');
-console.log('firebase:' + firebase);
+
 
 const auth = firebase.initializeApp(config.firebaseConfig).auth();
 const db = firebase.database();
-console.log('firebase:' + firebase);
+
 
 
 module.exports = { auth , db};
